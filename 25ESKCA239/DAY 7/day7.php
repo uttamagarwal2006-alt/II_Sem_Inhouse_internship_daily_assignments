de8d0886e3b6d6a5c6f62e5c1a1402d688d3f4e2
Uttam Agarwal <uttamagarwal2006@gmail.com>
2026-07-12 17:10:55 +0530

    Create day7.php

---
+++ 25ESKCA239/DAY 7/day7.php
@@ -0,0 +1,8 @@
+<?php
+$name = $_GET["name"];
+$branch = $_GET["branch"];
+$email = $_GET["mail"];
+$phoneNumber = $_GET["phoneNumber"];
+
+echo "Values received : $name $branch $email $phoneNumber ";
+?>
