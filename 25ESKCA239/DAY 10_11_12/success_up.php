889c25c0741f8abc992b5f068d51e439a20600e1
Uttam Agarwal <uttamagarwal2006@gmail.com>
2026-07-12 22:45:36 +0530

    Add success_up.php for update confirmation page

---
+++ 25ESKCA239/DAY 10_11_12/success_up.php
@@ -0,0 +1,16 @@
+<?php
+include ("header.php");
+?>
+
+<div class="container mt-5 text-center">
+    <div class="alert alert-success">
+        <h3>Updated Successful!</h3>
+        <p>Your form has been updated successfully.</p>
+    </div>
+
+    <a href="dashboard.php" class = "btn btn-primary">Go Back</a>
+</div>
+
+<?php
+include("fotter.php");
+?>
