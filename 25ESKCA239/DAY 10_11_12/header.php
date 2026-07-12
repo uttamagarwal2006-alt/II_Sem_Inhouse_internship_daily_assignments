507a59cc21c90cdc1d0d2d08bcebc210ef6a212c
Uttam Agarwal <uttamagarwal2006@gmail.com>
2026-07-12 22:41:49 +0530

    Add header.php with HTML structure and navigation

---
+++ 25ESKCA239/DAY 10_11_12/header.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <body>
+    <header class="bg-light border-bottom">
+        <div class="container">
+            <div class="d-flex justify-content-between align-items-center py-3">
+
+                <!-- Logo -->
+                <img src="images (1).jpg" alt="Logo" width="80">
+
+                <!-- Navigation -->
+                <nav>
+                    <ul class="nav">
+                        <li class="nav-item">
+                            <a href="home.php"
+                            class="nav-link text-dark" >Home</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a href="AboutUs.php"
+                            class="nav-link text-dark" >About US</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a href="contact.php"
+                            class="nav-link text-dark" >Contact US</a>
+                        </li>
+    
+                    </ul>
+                </nav>
+                    <button type = "button" class="btn btn-primary" onclick="window.location.href='login.php'"> log in</button>
+                
+            </div>
+        </div>
+    </header>
+</body>
+</html>
