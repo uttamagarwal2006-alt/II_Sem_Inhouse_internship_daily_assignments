be982284e03bc88768eb5ae58d9a9c7ad14489dd
Uttam Agarwal <uttamagarwal2006@gmail.com>
2026-07-12 22:39:15 +0530

    Add dashcontent.php with navigation links

---
+++ 25ESKCA239/DAY 10_11_12/dashcontent.php
@@ -0,0 +1,14 @@
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-3">
+    
+        <a href="updatePassword.php" class="btn btn-primary mb-3">Update Password</a>
+        <br>
+        <a href="updateprofile.php" class="btn btn-primary pt-2">Update Profile</a>
+        
+    </div>
+<div class="col-md-9 pt-4">
+
+    <!-- <div class="p-4 mb-4 bg-light rounded shadow-sm text-center">
+    <div class="d-flex justify-content-center gap-3"> -->
+    <!-- <a href="logout.php" class="btn btn-outline-danger px-4">Logout</a> -->
