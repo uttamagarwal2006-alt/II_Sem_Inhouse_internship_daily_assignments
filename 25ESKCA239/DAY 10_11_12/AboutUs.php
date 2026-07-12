d445c0c5226fe547255ad21c985d530982a96f28
Uttam Agarwal <uttamagarwal2006@gmail.com>
2026-07-12 22:34:48 +0530

    Add About Us page with mission and vision statements

---
+++ 25ESKCA239/DAY 10_11_12/AboutUs.php
@@ -0,0 +1,32 @@
+<?php
+include("header.php");
+?>
+
+<div class="container mt-5">
+    <h2>About Us</h2>
+
+    <p>
+        We are passionate about creating simple, responsive and user-friendly
+        websites. Our goal is to provide quality work while continuously
+        learning new technologies.
+    </p>
+
+    <h4>Our Mission</h4>
+
+    <ul>
+        <li>Provide quality web solutions.</li>
+        <li>Create responsive websites.</li>
+        <li>Ensure customer satisfaction.</li>
+    </ul>
+
+    <h4>Our Vision</h4>
+
+    <p>
+        To become a trusted platform that delivers modern and innovative
+        web solutions.
+    </p>
+</div>
+
+<?php
+include("fotter.php");
+?>
