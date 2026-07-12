4de57acc0a22325eda2f9cd7c2c5bf4068848e2d
Uttam Agarwal <uttamagarwal2006@gmail.com>
2026-07-12 22:42:27 +0530

    Create home.php

---
+++ 25ESKCA239/DAY 10_11_12/home.php
@@ -0,0 +1,24 @@
+<?php
+include("header.php");
+?>
+
+<div class="container mt-5 text-center">
+    <h1>Welcome to Our Website</h1>
+
+    <p class="lead">
+        We are glad to have you here. Our website is designed to provide
+        useful information and quality services in a simple and user-friendly way.
+    </p>
+
+    <p>
+        Explore our website to learn more about us, our services,
+        and feel free to contact us for any questions.
+    </p>
+
+    <a href="AboutUs.php" class="btn btn-primary">Know More</a>
+    <a href="registration.php" class="btn btn-primary">Go back</a>
+</div>
+
+<?php
+include("fotter.php");
+?>
